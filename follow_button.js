e10da08f0b54b17bd5b74532aa90c23f484e00ba
Daily-manjoo <yuslz35@gmail.com>
2023-05-14 00:00:47 +0900

    0514 commit

--- follow_button.js
+++ follow_button.js
@@ -1,7 +1,42 @@
 // React Component!
 // Follow <-> following
 function FollowButton() {
-  return React.createElement("div", {}, "Follow");
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: "12px",
+    right: "16px",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold",
+  };
+
+  const followBtnStyle = {
+    ...commonBtnStyle, //commonBtnStyle 객체를 스프레드해서 스타일 추가
+    backgroundColor: "black",
+    color: "white",
+  };
+
+  const followingBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid #cfd9de",
+  };
+
+  return React.createElement(
+    "div",
+    {
+      onClick: () => setFollowing(!following),
+      style: following ? followingBtnStyle : followBtnStyle,
+    },
+    following ? "following" : "Follow"
+  );
 }
 
 const domContainer = document.querySelector("#follow_button_container");
